0ba2f82017f64fa5fefcaa8c1677db24da0f3af2
Cvethencourt <135485800+Cvethencourt@users.noreply.github.com>
2023-06-26 15:47:06 -0400

    Se agrega una solicitud de datos al usuario

--- Sudoku(ProyectoII).pas
+++ Sudoku(ProyectoII).pas
@@ -1,7 +1,11 @@
 program Proyectosudoku;
 
 uses crt;
-var opcion : integer;
+var 
+opcionjuego,userage, opciondatos: integer; 
+username, useremail:string [30]; 
+userphone : string [11]; 
+
 
 Procedure instrucciones;
 Begin
@@ -25,7 +29,46 @@ End;
 
 
 BEGIN
-	instrucciones;
-	
+	Writeln ('      Bienvenido al juego de sudoku      ');
+	Writeln ('Por favor ingrese sus datos para comenzar');
+	Writeln ('-----------------------------------------');
+	repeat 
+		Writeln ('Por favor ingrese su nombre');
+		Readln (username);
+		Writeln ('Por favor ingrese su edad');
+		Readln (userage);
+		Writeln ('Por favor ingrese su direccion de correo electronico');
+		Readln (useremail);
+		Writeln ('Por favor ingrese su numero telefonico');
+		Readln (userphone);
+		clrscr;
+		writeln ('Los datos ingresados son:');
+		Writeln ('Nombre: ', username);
+		writeln ('Edad: ', userage);
+		writeln ('Correo: ', useremail);
+		writeln ('Telefono: ', userphone);
+		Writeln ();
+		writeln ('Los datos son correctos?');
+		writeln ('En caso de que Si, teclee 1');
+		Writeln ('En caso de que No, teclee 2 para editarlos');
+		Readln (opciondatos);
+	until opciondatos = 1;
+	clrscr;
+	Writeln ('Bienvenid@ ', username);
+	writeln ('Indique su opcion');
+	writeln ('=================');
+	writeln ('1 = Jugar');
+	Writeln ('2 = Mostrar reglas');
+	Writeln ('3 = Salir');
+	Readln (opcionjuego);
+		case opcionjuego of 
+			1: begin
+			end;
+			2: begin
+			instrucciones;
+			end;
+			3: begin
+			end;
+		end;
 END.
 
